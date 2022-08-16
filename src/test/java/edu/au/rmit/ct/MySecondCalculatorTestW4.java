7c83c023105ac12bc05e3a564ca646f694b9ab3a
isunrmit <ivan.sun@rmit.edu.au>
2022-08-16 10:08:12 +1000

    W4 update

---
+++ src/test/java/edu/au/rmit/ct/MySecondCalculatorTestW4.java
@@ -0,0 +1,20 @@
+package edu.au.rmit.ct;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MySecondCalculatorTestW4{
+
+    @Test
+    @DisplayName("Get Memory STore")
+    void getMemoryStore() {
+    }
+
+    @Test
+    void resetMemory() {
+    }
+
+    @Test
+    void pretendToThink() {
+    }
+}
