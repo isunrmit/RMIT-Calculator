4bb9a5a2e9af369844028523a30b47ce97f9108b
isunrmit <ivan.sun@rmit.edu.au>
2022-08-16 10:06:32 +1000

    W5 update

--- src/test/java/edu/au/rmit/ct/MyCalculatorW4Test_Soln.java
+++ src/test/java/edu/au/rmit/ct/MyCalculatorW4Test_Soln.java
@@ -5,15 +5,16 @@ import org.junit.jupiter.api.*;
 
 import java.time.Duration;
 
-public class MyCalculatorW4Test {
+public class MyCalculatorW4Test_Soln {
 
     @Test
     @DisplayName("Example of testing the default constructor with no params")
     void checkDefaultConstructor() {
         MySecondCalculator msc999 = new MySecondCalculator();
         // check ID and name with asserts
-        fail("Not implemented yet");
-
+        // fail("Not implemented yet");
+        assertEquals(999 , msc999.getId());
+        assertEquals( "Test Model", msc999.getName());
 
     }
 
@@ -21,27 +22,40 @@ public class MyCalculatorW4Test {
     @DisplayName("Example of AssertTrue @DisplayName / AssertFalse and Reset Memory")
     void checkBooleanReturnResetMemory() throws Exception {
         MySecondCalculator msc300 = new MySecondCalculator(300, "Advanced Model");
-        fail("Not implemented yet");
+
+        //
+        // fail("Not implemented yet");
 
         // Step 1. reset memory of the calculator. Use an assert method to check this was done correctly
+        assertFalse(msc300.resetMemory());
 
         // Step 2. check value of adding 29 + 1. you should get an expected result, also
-
+        assertEquals(30, msc300.addIntegers(29,1));
         // Step 3. it means that you will have saved something to the memory store.
         // Check that with an asserts
+        assertEquals(30, msc300.getMemoryStore());
 
         // Step 4. reset memory, and check with asserts this was done as expected.
-
+        assertEquals(true, msc300.resetMemory());
+        assertEquals( 0, msc300.getMemoryStore() );
     }
     @Test
     void pretendToThink() throws Exception {
         MySecondCalculator msc300 = new MySecondCalculator(300, "Advanced Model");
-        fail("not implemented yet. Run me first to see what i do");
+        assertTimeout( Duration.ofMillis(2405) ,()-> msc300.pretendToThink(2400));
+    }
+    @Test
+    @RepeatedTest(10)
+    void pretendToThinkMultiple() throws Exception {
+        MySecondCalculator msc300 = new MySecondCalculator(300, "Advanced Model");
+        assertTimeout( Duration.ofMillis(2405) ,()-> msc300.pretendToThink(2400));
     }
 
     @Test
     @DisplayName("Check Exception thrown when calling Constructor with reserved ID number (999)")
-    void checkExceptionThrownConstructor() {
+    void checkExceptionThrownConstructor() throws Exception {
+        // Extra extension question.
         fail("not implemented yet");
-    }
+
+     }
 }
---
+++ src/test/java/edu/au/rmit/ct/MySecondCalculatorTestW5.java
@@ -0,0 +1,39 @@
+package edu.au.rmit.ct;
+
+import org.junit.jupiter.api.*;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+// @TestMethodOrder(MethodOrderer.MethodName.class)
+// @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class MySecondCalculatorTestW5 {
+
+    @Test
+    //@Order(4)
+    @DisplayName("Check Exception thrown when calling Constructor with reserved ID number (999)")
+    void checkMessageOfExceptionThrown() throws Exception {
+        // MySecondCalculator msc300 = new MySecondCalculator(999, "Advanced Model");
+   }
+
+    @Test
+    // @Order(3)
+    @DisplayName("0. Demo of Ordering")
+    void test0() throws Exception {
+        fail("not implemented");
+    }
+
+
+
+    @Test
+    // @Order(2)
+    @DisplayName("1. Demo of Ordering")
+    void test1() throws Exception {
+        fail("not implemented");
+   }
+    @Test
+    // @Order(1)
+    @DisplayName("2. Demo of Ordering")
+    void test2() throws Exception {
+        fail("not implemented");
+    }
+}
